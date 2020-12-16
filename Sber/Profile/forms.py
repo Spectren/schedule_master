463fe629c3571d002c9660ee06a55d048af426c0
Spectren <ignatov-e@inbox.ru>
2020-12-17 01:40:29 +0500

    Add models

--- Sber/Profile/forms.py
+++ Sber/Profile/forms.py
@@ -1,25 +1,45 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ImageField, FileInput, DateInput, CharField, HiddenInput
 from django.contrib.auth.models import User
 from django_registration.forms import RegistrationFormUniqueEmail
-from .models import MentorData
+from .models import MentorData, TrainerData
 
 class UserFormMixin(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'first_name', 'last_name']
 
 class ProfileFormMixin(ModelForm):
     class Meta:
         model = MentorData
         exclude = ['id']
 
+class TrainerFormMixin(ModelForm):
+    class Meta:
+        model = TrainerData
+        exclude = ['id']
+
+class MixedRegistrationTrainerForm(UserFormMixin, ProfileFormMixin, TrainerFormMixin, RegistrationFormUniqueEmail):
+    mentor = CharField(widget=HiddenInput())
+
 class MixedRegistrationForm(UserFormMixin, ProfileFormMixin, RegistrationFormUniqueEmail):
     pass
+
+class CustomClearableFileInput(FileInput):
+    is_initial=False
+
+class CustomDateInput(DateInput):
+    input_type="date"
 
 class EditMentorForm(ModelForm):
     class Meta:
         model  = MentorData
         exclude = ['id', 'owner']
+
+        widgets = {
+            "avatar": CustomClearableFileInput(),
+            "birthday": CustomDateInput()
+        }
+
 
 class EditUserForm(ModelForm):
     class Meta:
