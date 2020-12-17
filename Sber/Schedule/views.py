b3b584332c2f88e741831183d89c93946a1682d4
BigBrownBag <72513891+BigBrownBag@users.noreply.github.com>
2020-12-18 00:08:24 +0500

    change after merge

--- Sber/Schedule/views.py
+++ Sber/Schedule/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD:Sber/Schedule/views.py
 def schedule_views(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
@@ -8,11 +7,3 @@ def schedule_views(request):
     return render(request, 'schedule.html')
 
 
-=======
-
-def schedule_views(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-
-    return render(request, 'shedule.html')
->>>>>>> master:Sber/Shedule/views.py
